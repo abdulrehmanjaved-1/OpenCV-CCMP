a72fd487e75fb8ba3fb18fd1d342c18891d309bc
abdulrehmanjaved-1 <abdulrehmanjaveds12@gmail.com>
2024-10-06 16:19:23 +0500

    updated training data and resources

---
+++ faces_train.py
@@ -0,0 +1,8 @@
+import os
+import cv2 as cv
+import numpy as np
+
+# list of people which we will use to train opencv recognizer model on
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+Dir = r'C:\Users\IT-Support\Desktop\opencv\resources\Faces\train'
+
